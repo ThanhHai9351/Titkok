6c8dadc3d2b1fb3515b7abe3341a78339d22afb1
ThanhHai9351 <haihailua123456@gmail.com>
2023-09-26 00:33:25 +0700

    U

--- src/config-overrides.js
+++ src/config-overrides.js
@@ -1,28 +1,7 @@
-const {
-  override,
-} = require("customize-cra");
-const path = require("path");
+
+
+const {override, useBabelRc} = require("customize-cra");
 
 module.exports = override(
-  // enable legacy decorators babel plugin
-  addDecoratorsLegacy(),
-
-  // disable eslint in webpack
-  disableEsLint(),
-
-  // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
-  process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
-
-  // add an alias for "ag-grid-react" imports
-  addWebpackAlias({
-    ["ag-grid-react$"]: path.resolve(__dirname, "src/shared/agGridWrapper.js")
-  }),
-
-  // adjust the underlying workbox
-  adjustWorkbox(wb =>
-    Object.assign(wb, {
-      skipWaiting: true,
-      exclude: (wb.exclude || []).concat("index.html")
-    })
-  )
+    useBabelRc() 
 );
